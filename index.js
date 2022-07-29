8270d6d385130df3cea9e022ee97dfcb94750154
biancode <klaus@bianco-royal.de>
2022-07-29 12:43:30 +0200

    fix: missing index file

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ Copyright (c) 2017-2022 Klaus Landsdorf (http://node-red.plus/)
+ All rights reserved.
+ node-red-contrib-bit
+
+ @author <a href="mailto:klaus.landsdorf@bianco-royal.de">Klaus Landsdorf</a> (Bianco Royal)
+ **/
+'use strict'
